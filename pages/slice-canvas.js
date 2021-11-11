a3761c85bcfc10b52c9bd30093db2e4962f63d16
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2021-11-10 15:20:29 -1000

    feat: add Text slice

--- pages/slice-canvas.js
+++ pages/slice-canvas.js
@@ -1,6 +1,7 @@
 import { SliceCanvasRenderer } from "@prismicio/slice-canvas-renderer-react";
 import { SliceZone } from "@prismicio/react";
 
+import { components } from "../slices";
 import state from "../.slicemachine/slice-canvas-state.json";
 
 const SliceCanvas = () => (
