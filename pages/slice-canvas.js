372d8286ac2d6f09edc3c9648ab8839ba6e703cd
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2021-11-10 14:38:05 -1000

    feat: add Slice Canvas renderer

---
+++ pages/slice-canvas.js
@@ -0,0 +1,15 @@
+import { SliceCanvasRenderer } from "@prismicio/slice-canvas-renderer-react";
+import { SliceZone } from "@prismicio/react";
+
+import state from "../.slicemachine/slice-canvas-state.json";
+
+const SliceCanvas = () => (
+  <SliceCanvasRenderer
+    sliceZone={(slices) => (
+      <SliceZone slices={slices} components={components} />
+    )}
+    state={state}
+  />
+);
+
+export default SliceCanvas;
