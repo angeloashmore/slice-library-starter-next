1eb969c75bffe40c12a107f10225ab606ed0c2cd
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2021-11-10 16:01:38 -1000

    chore: remove all non-starter files

--- .slicemachine/assets/slices/Text/index.stories.js
+++
@@ -1,9 +0,0 @@
-import MyComponent from '../../../../slices/Text';
-
-export default {
-  title: 'slices/Text'
-}
-
-
-export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"text","items":[],"primary":{"title":[{"type":"heading1","text":"Benchmark bleeding-edge e-commerce","spans":[]}],"description":[{"type":"paragraph","text":"Mollit adipisicing aliqua mollit do minim reprehenderit dolore excepteur id. Voluptate est velit cillum ullamco consequat exercitation culpa ex.","spans":[]}]},"id":"_DefaultSlice"}} />
-_DefaultSlice.storyName = 'Default slice'
